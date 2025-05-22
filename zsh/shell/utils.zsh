ea39546609be282123b33111bd040b0a04a9d4b0
Harrison Cramer <harrisoncramer@gmail.com>
2025-05-22 16:57:49 -0400

    Remove personal RiverUI

--- zsh/shell/utils.zsh
+++ zsh/shell/utils.zsh
@@ -11,16 +11,3 @@ uuid() {
 iso () {
   date -u +"%Y-%m-%dT%H:%M:%SZ" | tr -d '\n' | pbcopy
 }
-
-function riverui () {
-  if [ -z "$1" ]; then
-    echo "Must provide service, e.g. riverui compliance"
-    exit 1
-  fi
-
-  echo 'Shutting down coauth service, which also runs on :8080'
-  SVC=$1
-  dc down coauth
-
-  DATABASE_URL="$DEV_DATABASE_URL&search_path=public,$SVC" ~/tools/riverui
-}
