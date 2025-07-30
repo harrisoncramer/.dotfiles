a91a1f09ac5562250be57a0313f41c987fffb0a6
Harrison Cramer <harrisoncramer@gmail.com>
2025-07-30 10:04:28 -0400

    Move diff stuff, etc.

--- zsh/shell/docker.zsh
+++ zsh/shell/docker.zsh
@@ -22,23 +22,24 @@ start() {
 }
 
 logs () {
-  logfile="/tmp/logs_buffer.txt"
-  rm -f "$logfile"  # Clear previous logs
-  touch "$logfile"
-
-  (dc logs --no-log-prefix "$@" -f >> "$logfile" 2>/dev/null) &
-  pid=$!
-
-  # Start in tailing mode by default
-  tail -n 100000 -f "$logfile" | fzf --tac --no-sort --exact --wrap \
-      --bind 'tab:toggle' \
-      --bind "ctrl-e:execute:echo {} | awk -F':' '{file=substr(\$1, 2); print \"+\"\$2, ENVIRON[\"MONO_DIR\"]file}' | xargs /Users/harrisoncramer/.local/bin/nvim-macos/bin/nvim" \
-      --bind 'enter:execute:echo {} | pbcopy' \
-      --bind 'esc:abort' \
-      --bind 'ctrl-t:reload(cat '"$logfile"')' \
-      --bind 'ctrl-r:reload(tail -n 100000 -f '"$logfile"')'
-
-  pkill $pid
+  mise run docker:logs "$@"
+  # logfile="/tmp/logs_buffer.txt"
+  # rm -f "$logfile"  # Clear previous logs
+  # touch "$logfile"
+  #
+  # (dc logs --no-log-prefix "$@" -f >> "$logfile" 2>/dev/null) &
+  # pid=$!
+  #
+  # # Start in tailing mode by default
+  # tail -n 100000 -f "$logfile" | fzf --tac --no-sort --exact --wrap \
+  #     --bind 'tab:toggle' \
+  #     --bind "ctrl-e:execute:echo {} | awk -F':' '{file=substr(\$1, 2); print \"+\"\$2, ENVIRON[\"MONO_DIR\"]file}' | xargs /Users/harrisoncramer/.local/bin/nvim-macos/bin/nvim" \
+  #     --bind 'enter:execute:echo {} | pbcopy' \
+  #     --bind 'esc:abort' \
+  #     --bind 'ctrl-t:reload(cat '"$logfile"')' \
+  #     --bind 'ctrl-r:reload(tail -n 100000 -f '"$logfile"')'
+  #
+  # pkill $pid
 }
 
 attach() {
