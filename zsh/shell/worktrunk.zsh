7020b30c6ec34adba076efe8f4a1d610b31c0815
Harrison Cramer <harrisoncramer@gmail.com>
2026-04-03 16:38:19 -0400

    Updated zsh/shell/worktrunk.zsh

--- zsh/shell/worktrunk.zsh
+++ zsh/shell/worktrunk.zsh
@@ -1,7 +1,8 @@
 alias wts="wt switch"
 
 function wtc () {
-  NAME=$(gum input --placeholder "Worktree name, e.g. the feature")
+  NAME=$(gum input --placeholder "Worktree name, e.g. the feature") || return
+  [[ -z "$NAME" ]] && return
   wt switch --create "harry/$NAME"
 }
 
