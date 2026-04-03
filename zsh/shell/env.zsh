97683c37e474694a4c18d454dea8b48a722a4989
Harrison Cramer <harrisoncramer@gmail.com>
2026-04-03 10:10:15 -0400

    fixing up standup commands

--- zsh/shell/env.zsh
+++ zsh/shell/env.zsh
@@ -25,21 +25,21 @@ if [ "$HOST_NAME" = "harry-work-computer" ]; then
     GITHUB_TOKEN=$(op item get 'Github API Token' --fields 'api_token' --reveal)
     export GITHUB_TOKEN=$GITHUB_TOKEN
   fi
-  if [ -z "$PROD_DATABASE_URL" ]; then
-    export PROD_DATABASE_URL=$(getSecret 'Mise Secrets' 'Development' 'PROD_DATABASE_URL')
-  fi
-
-  if [ -z "$STAGING_DATABASE_URL" ]; then
-    export STAGING_DATABASE_URL=$(getSecret 'Mise Secrets' 'Development' 'STAGING_DATABASE_URL')
-  fi
-
-  if [ -z "$PROD_READ_ONLY_DATABASE_URL" ]; then
-    export PROD_READ_ONLY_DATABASE_URL=$(getSecret 'Mise Secrets' 'Development' 'PROD_READ_ONLY_DATABASE_URL')
-  fi
-
-  if [ -z "$SANDBOX_DATABASE_URL" ]; then
-    export SANDBOX_DATABASE_URL=$(getSecret 'Mise Secrets' 'Development' 'SANDBOX_DATABASE_URL')
-  fi
+  # if [ -z "$PROD_DATABASE_URL" ]; then
+  #   export PROD_DATABASE_URL=$(getSecret 'Mise Secrets' 'Development' 'PROD_DATABASE_URL')
+  # fi
+  #
+  # if [ -z "$STAGING_DATABASE_URL" ]; then
+  #   export STAGING_DATABASE_URL=$(getSecret 'Mise Secrets' 'Development' 'STAGING_DATABASE_URL')
+  # fi
+  #
+  # if [ -z "$PROD_READ_ONLY_DATABASE_URL" ]; then
+  #   export PROD_READ_ONLY_DATABASE_URL=$(getSecret 'Mise Secrets' 'Development' 'PROD_READ_ONLY_DATABASE_URL')
+  # fi
+  #
+  # if [ -z "$SANDBOX_DATABASE_URL" ]; then
+  #   export SANDBOX_DATABASE_URL=$(getSecret 'Mise Secrets' 'Development' 'SANDBOX_DATABASE_URL')
+  # fi
 
   if [ -z "$DEV_DATABASE_URL" ]; then
     export DEV_DATABASE_URL="postgresql://chariot:samplepassword@0.0.0.0:5432/chariot?connect_timeout=300"
